8fc3f42eefb6a10edce3454c441a5beb0df4e360
sivass <sivass2703@gmail.com>
2025-12-16 11:01:32 +0800

    updated the server,app and handler to SRP way and minimal clean

--- backend/services/general/src/server.ts
+++ backend/services/general/src/server.ts
@@ -1,25 +1,7 @@
-import express from "express";
-import cookieParser from "cookie-parser";
-import bodyParser from "body-parser";
-import cors from "cors";
-import subscriberRoutes from "./interfaces/routes/subscriber.routes";
-import contactRoutes from "./interfaces/routes/contact.routes";
+import { app } from "./app";
+import config from "./config";
 
-const app = express();
-app.use(cookieParser());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({
-  origin: ['http://localhost:3000'],
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-app.use(express.json());
-
-// Register routes
-app.use("/api/subscribe", subscriberRoutes);
-app.use("/api/contact", contactRoutes);
-
-const PORT = process.env.PORT || 4000;
+const PORT = config.port || 4000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
