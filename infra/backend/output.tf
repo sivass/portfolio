1013f7313af31efa2b1ac24edf621e2218c4f1a1
sivass <sivass2703@gmail.com>
2025-12-16 18:05:19 +0800

    infra(backend) API gateway url to custom url mapped for backend api

--- infra/backend/output.tf
+++ infra/backend/output.tf
@@ -1,4 +1,9 @@
 output "api_url" {
-    description = "The URL of the API Gateway"
-    value       = aws_apigatewayv2_api.api.api_endpoint
+  description = "The URL of the API Gateway"
+  value       = aws_apigatewayv2_api.api.api_endpoint
+}
+
+output "custom_domain_url" {
+  description = "The URL of the API using custom domain"
+  value       = "https://${var.domain_name}"
 }
